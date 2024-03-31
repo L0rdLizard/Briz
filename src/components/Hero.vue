3486d53bee8fee4cc2c3548d4d6684b578626d8f
L0rdLizard <WarriorXKZ@gmail.com>
2024-03-31 19:26:18 +0300

    add last 2 buttons

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,35 +1,47 @@
 <template>
     <div class="container-fluid g-0">
-        <img style="object-fit: cover;" class="img-fluid w-100 min-vh-25 min-vh-md-50 mb-n7 custom-image"
-            src="/airplane.png" alt="Photo by Irene Dávila">
-    </div>
-
-    <div class="container pt-4 ps-4 bg-body mt-md-n6 position-relative rounded-5 shadow bg-main2 custom-div"
-        style="margin-bottom: 100px;">
-
-        <TabSelector />
-
-
-        <!-- <div class="row">
-            <div class="col-md-4 text-center align-self-center">
-                <div class="lc-block border-lg-end border-2 ">
-                    <div editable="rich">
-                        <p class="display-4 text-secondary">WHY BRIZ?</p>
-                    </div>
+        <!-- <img style="object-fit: cover;" class="img-fluid w-100 min-vh-25 min-vh-md-50 mb-n7 custom-image"
+            src="/airplane.png" alt="Photo by Irene Dávila"> -->
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://picsum.photos/1200/350" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://picsum.photos/1200/350" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://picsum.photos/1200/350" class="d-block w-100" alt="...">
                 </div>
             </div>
-            <div class="col-md-8">
-                <div class="lc-block ">
-                    <div editable="rich">
-                        <p class="display-4">Think different and create a wonderful kind of digital agency.</p>
-                    </div>
-                </div>
-            </div>
-        </div> -->
+            <button class="col carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
 
-
+            <button class="col carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
 
     </div>
+
+    <div class="col container">
+        <div class="row">
+
+
+
+        </div>
+        <div class="row container pt-4 ps-4 bg-body mt-md-n6 position-relative rounded-5 shadow custom-div">
+
+            <TabSelector />
+
+        </div>
+    </div>
+
 </template>
 
 <script lang="ts" setup>
@@ -104,7 +116,7 @@ import TabSelector from "@/components/TabSelector.vue";
     }
 
     .custom-div {
-        margin-top: 50px;
+        margin-top: -50px;
     }
 }
 </style>
--- src/components/TabSelector.vue
+++ src/components/TabSelector.vue
@@ -3,15 +3,15 @@
         <div class="row m-0">
             <div class="col-9 square-container rounded-5 p-1 menu1">
                 <div class="btn-group container-fluid p-1" role="group" aria-label="Basic radio toggle button group">
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                    <input type="radio" class="btn-check" name="btnradio1" id="btnradio1" autocomplete="off" checked>
                     <label class="btn btn-outline-primary rounded-5 custom-btn item active" for="btnradio1">Купить
                         билет</label>
 
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                    <input type="radio" class="btn-check" name="btnradio1" id="btnradio2" autocomplete="off">
                     <label class="btn btn-outline-primary rounded-5 custom-btn item"
                         for="btnradio2">Онлайн-регистрация</label>
 
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                    <input type="radio" class="btn-check" name="btnradio1" id="btnradio3" autocomplete="off">
                     <label class="btn btn-outline-primary rounded-5 custom-btn item" for="btnradio3">Управление
                         бронированием</label>
                 </div>
@@ -42,7 +42,8 @@
 
                         <!-- Подвал модального окна -->
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal">Закрыть</button>
+                            <button type="button" class="btn btn-outline-primary"
+                                data-bs-dismiss="modal">Закрыть</button>
                         </div>
 
                     </div>
@@ -55,7 +56,7 @@
             </button>
 
             <!-- Модальное окно -->
-            
+
 
             <div class="col d-flex justify-content-end align-items-center me-4">
                 <div class="form-check">
@@ -85,25 +86,51 @@
             <div class="col-4 square-container rounded-4 m-1 p-1 d-flex justify-content-center align-items-center">
                 <div class="row">
                     <input type="text" class="col form-control custom-input-group rounded-4 m-0 me-4 ms-2"
-                        placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                        placeholder="Откуда" aria-label="Username" aria-describedby="basic-addon1">
 
                     <input type="text" class="col form-control custom-input-group rounded-4 m-0 me-2 ms-4"
-                        placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                        placeholder="Куда" aria-label="Username" aria-describedby="basic-addon1">
                 </div>
 
             </div>
             <div class="col-2 square-container rounded-4 m-1 p-1 d-flex justify-content-center align-items-center">
+                <img src="/calendar-month.svg" alt="Описание иконки">
                 <input type="text" class="form-control custom-input-group m-0 rounded-4" placeholder="Туда"
                     aria-label="Username" aria-describedby="basic-addon1">
             </div>
             <div class="col-2 square-container rounded-4 m-1 p-1 d-flex justify-content-center align-items-center">
+                <img src="/calendar-month.svg" alt="Описание иконки">
                 <input type="text" class="form-control custom-input-group m-0 rounded-4" placeholder="Обратно"
                     aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+            <div class="col-2 square-container rounded-4 m-1 p-1 d-flex justify-content-center align-items-center">
+                <select class="form-select square-container custom-btn rounded-4" id="inputGroupSelect01"
+                    style="width: 100%; height: 100%;">
+                    <option selected>Choose...</option>
+                    <option value="1">1 клиент</option>
+                    <option value="2">2 клиента</option>
+                    <option value="3">2+ клиента</option>
+                </select>
+            </div>
+            <div class="col-1 square-container2 rounded-4 m-1 p-0 d-flex justify-content-center align-items-center">
+                <button type="button" class="square-container2 custom-btn p-0 m-0 rounded-4"
+                    style="width: 100%; height: 100%;">Найти</button>
             </div>
         </div>
     </div>
 
 </template>
+
+
+<script lang="ts" setup>
+import TabSelector from "@/components/TabSelector.vue";
+window.onload = function () {
+    var element = document.getElementById('btnradio1') as HTMLInputElement;
+    if (element) {
+        element.checked = true;
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 @media (max-width: 800px) {
@@ -129,12 +156,17 @@
 .custom-input-group {
     border-color: #e1f1f0;
     background-color: #e1f1f0;
-    color: #e1f1f0;
+    color: #134C67;
 }
 
 .square-container {
     padding: 10px;
     background-color: #e1f1f0;
+}
+
+.square-container2 {
+    padding: 10px;
+    background-color: #EF8B77;
 }
 
 .custom-btn {
