71892b8ee63144a39b08a0083ccb1842abc6c997
LordLizard Notebook <WarriorXKZ@gmail.com>
2024-05-11 00:55:59 +0300

    add photos

--- src/components/ChooseTicket.vue
+++ src/components/ChooseTicket.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container p-2 rounded-4" style="margin-top: 40px; background-color: white; " >
+    <div class="container p-2 rounded-4" style="margin-top: 40px; background-color: white; ">
         <div class="container-liquid row p-4 m-4 mt-3 rounded-4 custom-element1">
             <div class="container-liquid col-2">
                 <p class="custom-font inter-700 mt-1 mb-1" style="font-size: 36px;"> 9:15 </p>
@@ -11,16 +11,20 @@
                 <p class="mt-3">BR 0235 Boeing 737-800</p>
             </div>
 
-            <div class="container-liquid col-5 pt-3">
+            <div class="container-liquid col-5 pt-0">
                 <div class="row">
-                    <p class="col d-flex justify-content-start align-items-start custom-font inter-400 pb-2 mt-1 mb-1" 
-                    style="color: #9D9D9F;">в пути 2 ч</p>
+                    <p class="col d-flex justify-content-start align-items-start custom-font inter-400 ps-1 pb-2 mt-1 mb-1"
+                        style="color: #9D9D9F;">
+                        <img class="ms-2" src="/plane-departure.png" height="34" alt="Описание иконки">
+                    </p>
 
-                    <p class="col d-flex justify-content-center align-items-center custom-font inter-400 pb-2 mt-1 mb-1" 
-                    style="color: #9D9D9F;">в пути 2 ч</p>
+                    <p class="col d-flex justify-content-center align-items-center custom-font inter-400 pb-2 mt-1 mb-1"
+                        style="color: #9D9D9F;">в пути 2 ч</p>
 
-                    <p class="col d-flex justify-content-end align-items-end custom-font inter-400 pb-2 mt-1 mb-1" 
-                    style="color: #9D9D9F;">в пути 2 ч</p>
+                    <p class="col d-flex justify-content-end align-items-end custom-font inter-400 pb-2 mt-1 mb-1"
+                        style="color: #9D9D9F;">
+                        <img class="ms-2" src="/plane-arrival.png" height="34" alt="Описание иконки">
+                    </p>
                 </div>
                 <hr class="custom-hr bg-main1 border-color-main1" />
             </div>
@@ -154,8 +158,22 @@
                 <p class="mt-3">BR 0235 Boeing 737-800</p>
             </div>
 
-            <div class="container-liquid col-5 ">
+            <div class="container-liquid col-5 pt-0">
+                <div class="row">
+                    <p class="col d-flex justify-content-start align-items-start custom-font inter-400 ps-1 pb-2 mt-1 mb-1"
+                        style="color: #9D9D9F;">
+                        <img class="ms-2" src="/plane-departure.png" height="34" alt="Описание иконки">
+                    </p>
 
+                    <p class="col d-flex justify-content-center align-items-center custom-font inter-400 pb-2 mt-1 mb-1"
+                        style="color: #9D9D9F;">в пути 2 ч</p>
+
+                    <p class="col d-flex justify-content-end align-items-end custom-font inter-400 pb-2 mt-1 mb-1"
+                        style="color: #9D9D9F;">
+                        <img class="ms-2" src="/plane-arrival.png" height="34" alt="Описание иконки">
+                    </p>
+                </div>
+                <hr class="custom-hr bg-main1 border-color-main1" />
             </div>
 
             <div class="container-liquid col-2 " style="text-align: right;">
@@ -174,7 +192,7 @@
                         <button type="button" @click="toggleElements2"
                             class="square-container2 custom-btn1 p-3 m-2 mb-3 rounded-4 custom-font inter-700"
                             style="width: 100%; color: #FBFBFB; min-width: 234px;">
-                            {{ isVisible1 ? 'Скрыть' : 'Показать' }}
+                            {{ isVisible2 ? 'Скрыть' : 'Показать' }}
                         </button>
 
                     </div>
@@ -287,8 +305,22 @@
                 <p class="mt-3">BR 0235 Boeing 737-800</p>
             </div>
 
-            <div class="container-liquid col-5 ">
+            <div class="container-liquid col-5 pt-0">
+                <div class="row">
+                    <p class="col d-flex justify-content-start align-items-start custom-font inter-400 ps-1 pb-2 mt-1 mb-1"
+                        style="color: #9D9D9F;">
+                        <img class="ms-2" src="/plane-departure.png" height="34" alt="Описание иконки">
+                    </p>
 
+                    <p class="col d-flex justify-content-center align-items-center custom-font inter-400 pb-2 mt-1 mb-1"
+                        style="color: #9D9D9F;">в пути 2 ч</p>
+
+                    <p class="col d-flex justify-content-end align-items-end custom-font inter-400 pb-2 mt-1 mb-1"
+                        style="color: #9D9D9F;">
+                        <img class="ms-2" src="/plane-arrival.png" height="34" alt="Описание иконки">
+                    </p>
+                </div>
+                <hr class="custom-hr bg-main1 border-color-main1" />
             </div>
 
             <div class="container-liquid col-2 " style="text-align: right;">
@@ -307,7 +339,7 @@
                         <button type="button" @click="toggleElements3"
                             class="square-container2 custom-btn1 p-3 m-2 mb-3 rounded-4 custom-font inter-700"
                             style="width: 100%; color: #FBFBFB; min-width: 234px;">
-                            {{ isVisible1 ? 'Скрыть' : 'Показать' }}
+                            {{ isVisible3 ? 'Скрыть' : 'Показать' }}
                         </button>
                     </div>
                 </div>
