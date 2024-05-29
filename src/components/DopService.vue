ece565c49378c52bc7b4df9d19252179359f0346
L0rdLizard <WarriorXKZ@gmail.com>
2024-05-29 22:26:03 +0300

    finish

--- src/components/DopService.vue
+++ src/components/DopService.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="container" style="margin-bottom: 100px;">
+    <div class="container" style="margin-bottom: 100px; margin-top: 30px;">
 
         <div class="row">
-            <p class="m-4 mb-2 custom-text1 custom-color-text1 inter-700">ДОПОЛНИТЕЛЬНЫЕ УСЛУГИ</p>
-            <p class="m-4 mt-0 custom-text2 custom-color-text2 inter-700">ДЛЯ ВАШЕГО КОМФОРТНОГО ПУТЕШЕСТВИЯ</p>
+            <p class="m-4 mb-2 custom-text1 custom-color-style3 gunterz">ДОПОЛНИТЕЛЬНЫЕ УСЛУГИ</p>
+            <p class="m-4 mt-0 custom-text2 custom-color-text2 helio">ДЛЯ ВАШЕГО КОМФОРТНОГО ПУТЕШЕСТВИЯ</p>
             <div class="col-md pb-2">
                 <div class="card w-auto rounded-4 shadow" style="width: 18rem;">
                     <div class="ratio ratio-16x9" style="margin-top: -1px;">
                         <img src="/place.jpg" class="card-img-top rounded-4" alt="...">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Выбор места</h5>
-                        <p class="card-text mb-4">Выберите себе место в самолёте заранее!</p>
-                        <a href="#" class="btn btn-primary bg">Подробнее</a>
+                        <h5 class="card-title custom-font myriad">Выбор места</h5>
+                        <p class="card-text mb-4 custom-font myriad">Выберите себе место в самолёте заранее!</p>
+                        <a href="#" class="btn btn-primary bg myriad">Подробнее</a>
                     </div>
                 </div>
             </div>
@@ -23,9 +23,9 @@
                         <img src="/lunch.jpg" class="card-img-top rounded-4" alt="...">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Еда на заказ</h5>
-                        <p class="card-text mb-4">Выбор еды заранее</p>
-                        <a href="#" class="btn btn-primary bg">Подробнее</a>
+                        <h5 class="card-title custom-font myriad">Еда на заказ</h5>
+                        <p class="card-text mb-4 custom-font myriad">Выбор еды заранее</p>
+                        <a href="#" class="btn btn-primary bg myriad">Подробнее</a>
                     </div>
                 </div>
             </div>
@@ -36,9 +36,9 @@
                         <img src="/pet.jpg" class="card-img-top rounded-4" alt="...">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Перевозка питомцев</h5>
-                        <p class="card-text mb-4">Путешествие с питомцем</p>
-                        <a href="#" class="btn btn-primary bg">Подробнее</a>
+                        <h5 class="card-title custom-font myriad">Перевозка питомцев</h5>
+                        <p class="card-text mb-4 custom-font myriad">Путешествие с питомцем</p>
+                        <a href="#" class="btn btn-primary bg myriad">Подробнее</a>
                     </div>
                 </div>
             </div>
@@ -52,11 +52,11 @@
 <style lang="scss" scoped>
 // @import "../scss/variables.scss";
 .custom-text1 {
-    font-size: 2rem;
+    font-size: 30px;
 }
 
 .custom-text2 {
-    font-size: 1rem;
+    font-size: 16px;
 }
 
 .bg {
@@ -68,5 +68,9 @@
 .btn-primary:hover {
     background-color: #EF8B77;
     // background-color: $ral3022;
+}
+
+.custom-font{
+    color: #134C67;
 }
 </style>
